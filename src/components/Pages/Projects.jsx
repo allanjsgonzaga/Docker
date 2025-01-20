406406e5cb109247bc6d292e2842246b9770dde3
Allandev <allanjeje@gmail.com>
2025-01-20 17:45:26 -0300

    Modulação para os projetos
    Alterações para que quando os projetos forem acrescentados, ficar visivel na pagina inicial da pagina projetos.

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -2,6 +2,10 @@ import { useLocation } from "react-router-dom"
 
 import Message from "../layout/Message"
 
+import styles from 'D:/Users/ALLAN/Desktop/projetos/docker/Docker/src/components/Pages/Projects.module.css'
+import Container from '../layout/Container.jsx'
+import LinkButton from "../layout/LinkButton.jsx"
+
 function Projects(){
     const location = useLocation()
     let message = ''
@@ -10,9 +14,15 @@ function Projects(){
     }
 
     return (
-       <div>
-            <h1>Meus projetos</h1>
+       <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus projetos</h1>
+                <LinkButton to="/newproject" text="Criar projeto" />
+            </div>
             {message && <Message type = "success" msg={message}/>}
+            <Container>
+               <p>Projetos...</p> 
+            </Container>
        </div>
     )
 }
