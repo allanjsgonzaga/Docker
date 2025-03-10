d800124e0b18bbc0efe5182fd920ca25b014bd06
Allandev <allanjeje@gmail.com>
2025-03-10 15:50:33 -0300

    Att

--- src/components/Pages/Projects.jsx
+++ src/components/Pages/Projects.jsx
@@ -1,29 +1,61 @@
 import { useLocation } from "react-router-dom"
 
+import { useState, useEffect } from "react"
+
 import Message from "../layout/Message"
 
 import styles from 'D:/Users/ALLAN/Desktop/projetos/docker/Docker/src/components/Pages/Projects.module.css'
 import Container from '../layout/Container.jsx'
 import LinkButton from "../layout/LinkButton.jsx"
+import ProjectCard from "../project/ProjectCard.jsx"
 
-function Projects(){
+function Projects() {
+    const [projects, setProjects] = useState([])
+
     const location = useLocation()
     let message = ''
-    if(location.state){
+    if (location.state) {
         message = location.state.message
     }
 
-    return (
-       <div className={styles.project_container}>
-            <div className={styles.title_container}>
-                <h1>Meus projetos</h1>
-                <LinkButton to="/newproject" text="Criar projeto" />
-            </div>
-            {message && <Message type = "success" msg={message}/>}
-            <Container>
-               <p>Projetos...</p> 
-            </Container>
-       </div>
-    )
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+                headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(resp => resp.json())
+            .then(data =>{
+                setProjects(data)
+            })
+            .catch((err) =>console.log(err))
+        
+}, [])
+
+return (
+    <div className={styles.project_container}>
+        <div className={styles.title_container}>
+            <h1>Meus projetos</h1>
+            <LinkButton to="/newproject" text="Criar projeto" />
+        </div>
+        {message && <Message type="success" msg={message} />}
+        <Container customclass ="start">
+            {projects.length > 0 ? (
+                projects.map((project) => (
+                <ProjectCard 
+                id={project.id || "ID não disponível"}
+                name={project.name || "Nome não disponível"} 
+                budget={project.budget || "Orçamento não disponível"}
+                category={project.category.name || "Categoria não disponível"}
+                key={project.id || Math.random()}
+                />
+                ))
+            ):(
+                <p>Nenhum projeto encontrado.</p>
+            )}
+        </Container>
+    </div>
+)
 }
 export default Projects
